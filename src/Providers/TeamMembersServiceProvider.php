e09c081f5a504f811468ae0ac63fc2e2ee662f33
Dominik <sopic.dominik@gmail.com>
2026-01-28 14:02:57 -0500

    Bootstrap plugin with Themosis Service Provider

---
+++ src/Providers/TeamMembersServiceProvider.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace TeamMembersDirectory\Providers;
+
+use Themosis\Core\Support\ServiceProvider;
+
+class TeamMembersServiceProvider extends ServiceProvider{
+
+
+}
---
+++ team-members-directory.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Team Members Directory
+ * Description: Manage and display team members via shortcode using Themosis routes.
+ * Version: 0.1.0
+ * Author: Dominik Sopic
+ */
+
+defined('ABSPATH') || exit;
+
+require __DIR__ . '/bootstrap/app.php';
